92b99639c47540a95b384e34eb424337ed1693f5
Rasyidi - ROG <rasyidialwee@gmail.com>
2022-03-24 11:27:27 +0800

    belajar blade template

---
+++ resources/views/child1.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <p>ini adlaah contoh untuk blade templating menggunakan template inheritance</p>
+@endsection
--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -1,71 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Sistem Staff</title>
-</head>
+@section('content')
+    <div class="row">
+        <div class="col-10">
 
-<body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-10">
-
-                <h1>Senarai Staff</h1>
-            </div>
-            <div class="col-2">
-                <a href="/staffs/create" class="btn btn-outline-primary float-end">Create New</a>
-            </div>
+            <h1>Senarai Staff</h1>
         </div>
-
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
-        <div class="card">
-            <div class="card-body">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($staffs as $key => $staff)
-                            <tr>
-                                <td>{{ $staffs->firstItem() + $key }}</td>
-                                <td>{{ $staff->name }}</td>
-                                <td>
-                                    <div class="d-flex">
-                                        <a href="/staffs/{{ $staff->id }}/edit" class="btn mx-2 btn-warning">Edit</a>
-                                        <a href="/staffs/{{ $staff->id }}" class="btn mx-2 btn-info">View</a>
-                                        <form action="/staffs/{{ $staff->id }}" method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn mx-2 btn-danger" type="submit">Delete</button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="3">No Staff</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
-
-                {{ $staffs->links() }}
-            </div>
+        <div class="col-2">
+            <a href="/staffs/create" class="btn btn-outline-primary float-end">Create New</a>
         </div>
     </div>
-</body>
 
-</html>
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="card">
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($staffs as $key => $staff)
+                        <tr>
+                            <td>{{ $staffs->firstItem() + $key }}</td>
+                            <td>{{ $staff->name }}</td>
+                            <td>
+                                <div class="d-flex">
+                                    <a href="/staffs/{{ $staff->id }}/edit" class="btn mx-2 btn-warning">Edit</a>
+                                    <a href="/staffs/{{ $staff->id }}" class="btn mx-2 btn-info">View</a>
+                                    <form action="/staffs/{{ $staff->id }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn mx-2 btn-danger" type="submit">Delete</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="3">No Staff</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+
+            {{ $staffs->links() }}
+        </div>
+    </div>
+@endsection
